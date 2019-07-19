6e19be9730e6cca9729e6c322b7bc033882522ae
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-07-19 19:50:14 -0400

    [shaderpack] Add FileTree trait

--- src/loading/mod.rs
+++ src/loading/mod.rs
@@ -5,3 +5,53 @@
 //! of those and will instead only take in file paths and will return either streams of bytes or strings. The resource
 //! pack loader will also be able to read resource packs in either filesystem folders or a zip folder. It should be
 //! constructed in a way that will allow support for other zip formats
+
+use failure::Error;
+use futures::Future;
+use std::path::Path;
+
+trait FileTree<'a> {
+    type CreateResult: FileTree<'a>;
+    type DirIter: Iterator<Item = &'a Path>;
+
+    /// Create a file tree from the path provided.
+    /// May be expensive depending on the target you are opening.
+    fn from_path(path: &Path) -> Box<dyn Future<Output = Result<Self::CreateResult, Error>>>;
+
+    /// Checks is file path exists within the current file tree.
+    fn exists(&self, path: &Path) -> bool;
+
+    /// Checks if the path points to a file.
+    ///
+    /// File Exists -> `Some(true)`
+    /// Exists but isn't file -> `Some(false)`
+    /// Path doesn't exist -> `None`
+    fn is_file(&self, path: &Path) -> Option<bool>;
+
+    /// Checks if the path points to a directory.
+    ///
+    /// Directory Exists -> `Some(true)`
+    /// Exists but isn't directory -> `Some(false)`
+    /// Path doesn't exist -> `None`
+    fn is_dir(&self, path: &Path) -> Option<bool>;
+
+    /// Returns an iterator over all paths in the specified directory.
+    ///
+    /// Fails if the directory doesn't exist, or is unreadable.
+    fn read_dir(&self, path: &Path) -> Result<Self::DirIter, Error>;
+
+    /// Reads a file into a vector of u8.
+    ///
+    /// Fails if file doesn't exist or isn't readable.
+    fn read(&self, path: &Path) -> Box<dyn Future<Output = Result<Vec<u8>, Error>>>;
+
+    /// Reads a file as little endian into an array of u32.
+    ///
+    /// Fails if file doesn't exist or isn't readable.
+    fn read_u32(&self, path: &Path) -> Box<dyn Future<Output = Result<Vec<u32>, Error>>>;
+
+    /// Reads a file as little endian into an array of u32.
+    ///
+    /// Fails if file doesn't exist or isn't readable.
+    fn read_text(&self, path: &Path) -> Box<dyn Future<Output = Result<String, Error>>>;
+}
