c43e8472a7f29cc7370313db1c5021d001c7d49a
Janrupf <werbung.janrupf@t-online.de>
2019-07-19 19:57:45 +0200

    [surface] Remove options type and adjust docs

--- src/surface/mod.rs
+++ src/surface/mod.rs
@@ -1,27 +1,30 @@
 use failure::Fail;
 
 /// Represents an abstract Surface which provides the objects required for the rendering platform
-pub trait Surface<T, O> {
+///
+/// For windows this would very likely be a `HWND` (window handle), for Vulkan a `SurfaceKHR`.
+/// In the end however it is up to the render engine what it requests, but the 2 shipped with Nova
+/// will require the objects described above
+///
+/// Furthermore do the generic serve as compile time checks. For example, it will prevent that you
+/// can even pass a X11 window to DX12 in the code, as a X11 window won't implement
+/// `Surface<HWND>`
+pub trait Surface<T> {
     /// Creates or retrieves the object of the type `T` required for the current platform
-    ///
-    /// # Parameters
-    ///
-    /// * `options` - The options passed for retrieving the object
-    fn platform_object(options: O) -> Result<T, SurfaceError>;
+    fn platform_object() -> Result<T, SurfaceError>;
 }
 
 /// Errors that can occur during creation/access of the underlying platform object
 #[derive(Fail, Debug, Clone, Eq, PartialEq)]
 pub enum SurfaceError {
     #[fail(display = "Failed to create or access the underlying object")]
-    AccessFailed,
+    CreationOrAccessFailed,
 
-    #[fail(display = "Invalid parameters passed")]
-    InvalidParameters,
+    #[fail(display = "Invalid parameters passed: {}", details)]
+    InvalidParameters { details: String },
 
     // This is a special case and will usually not occur with the implementations provided
-    // by nova because the generics will prevent this
-    // H
+    // by Nova because the generics will prevent this
     #[fail(display = "This Surface can not be used for creating this object")]
     NotSupported,
 }
