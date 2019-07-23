e471b2bbbca610164ad24db56513c1fa5bccb7b0
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-07-23 12:53:02 -0400

    [loading] Use SingleThreadReactor in DirectoryFileTree

--- src/loading/dir.rs
+++ src/loading/dir.rs
@@ -1,25 +1,49 @@
+use crate::core::reactor::SingleThreadReactor;
 use crate::loading::{FileTree, LoadingError};
 use failure::Error;
 use futures::Future;
 use std::collections::HashMap;
-use std::io;
 use std::path::{Path, PathBuf};
+use std::slice;
 
-enum DirectoryEntry {
+enum FileSystemOp {
+    RecursiveEnumerate(PathBuf),
+}
+
+enum FileSystemOpResult {
+    RecursiveEnumerate(DirectoryCache),
+}
+
+pub enum DirectoryEntry {
     Directory(Vec<PathBuf>),
     File,
 }
 
-pub struct DirectoryFileTree {
+struct DirectoryCache {
     root: PathBuf,
     file_map: HashMap<PathBuf, DirectoryEntry>,
 }
 
-fn enumerate_directories_recursively(path: PathBuf) -> DirectoryEntry {}
+pub struct DirectoryFileTree {
+    cache: DirectoryCache,
+    reactor: SingleThreadReactor<FileSystemOp, FileSystemOpResult>,
+}
 
-impl FileTree<'_> for DirectoryFileTree {
+fn enumerate_directories_recursively(path: PathBuf) -> DirectoryCache {
+    unimplemented!()
+}
+
+fn file_system_reactor_core(op: FileSystemOp) -> FileSystemOpResult {
+    match op {
+        FileSystemOp::RecursiveEnumerate(path) => {
+            FileSystemOpResult::RecursiveEnumerate(enumerate_directories_recursively(path))
+        }
+    }
+}
+
+impl<'a> FileTree<'a> for DirectoryFileTree {
     type CreateResult = Self;
-    type DirIter = ();
+    type DirIter = DirectoryIterator<'a>;
 
     fn from_path(path: &Path) -> Box<dyn Future<Output = Result<Self::CreateResult, LoadingError>>> {
         let path = path.to_path_buf();
@@ -30,6 +54,14 @@ impl FileTree<'_> for DirectoryFileTree {
             if !path.is_dir() {
                 return Err(LoadingError::NotDirectory);
             }
+
+            let reactor = SingleThreadReactor::from_action(file_system_reactor_core);
+
+            let future = reactor.send_async(FileSystemOp::RecursiveEnumerate(path));
+
+            let FileSystemOpResult::RecursiveEnumerate(cache) = future.await;
+
+            Ok(Self { cache, reactor })
         })
     }
 
@@ -59,5 +91,21 @@ impl FileTree<'_> for DirectoryFileTree {
 
     fn read_text(&self, path: &Path) -> Box<dyn Future<Output = Result<String, Error>>> {
         unimplemented!()
+    }
+}
+
+pub struct DirectoryIterator<'a> {
+    subiter: slice::Iter<'a, PathBuf>,
+}
+
+impl<'a> Iterator for DirectoryIterator<'a> {
+    type Item = &'a Path;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.subiter.next().map(|v| v.as_path())
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.subiter.size_hint()
     }
 }
