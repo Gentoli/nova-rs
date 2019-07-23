70d444ecf96e1cc38a779faf1e96ae594fb60c70
David Dubois <dexcelstraun7@gmail.com>
2019-07-23 08:25:53 -0700

    [rhi::dx12] Use newly-merged official d3d12-rs

--- src/rhi/dx12/dx12_renderpass.rs
+++ src/rhi/dx12/dx12_renderpass.rs
@@ -1,5 +1,11 @@
 use crate::rhi::Renderpass;
 
-pub struct Dx12Renderpass {}
+/// DX12 renderpass yay
+///
+/// DX12 renderpasses are simpler than Vulkan renderpasses, because DX12 doesnt' care about mobile's tilled GPUs. This
+/// struct basically holds the parameters for a DX12 renderpass
+pub struct Dx12Renderpass {
+    // TODO: Give this struct members when `winapi-rs` support `ID3D12GraphicsCommandList4`
+}
 
 impl Renderpass for Dx12Renderpass {}
