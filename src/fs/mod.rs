c7b1304348db9d874a18b9974c2b07429c79d104
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-07-28 16:36:48 -0400

    [docs] Normalize Nova's capitalization

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,4 +1,4 @@
-//! File system utilities specific to nova's use case.
+//! File system utilities specific to Nova's use case.
 //!
 //! Not a general set of filesystem utils, but they are built on top of the stuff in [`std::io`] and
 //! [`std::fs`].
