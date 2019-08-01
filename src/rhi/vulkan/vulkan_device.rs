2e73aa327b2cac39df1944882385b618bfed207e
Janrupf <werbung.janrupf@t-online.de>
2019-08-01 18:01:49 +0200

    [rhi::vulkan] Implement create_pipeline_interface

--- src/rhi/vulkan/vulkan_device.rs
+++ src/rhi/vulkan/vulkan_device.rs
@@ -104,6 +104,70 @@ impl VulkanDevice {
         }
     }
 
+    fn nova_descriptor_type_to_vulkan_type(descriptor_type: DescriptorType) -> vk::DescriptorType {
+        match descriptor_type {
+            DescriptorType::CombinedImageSampler => vk::DescriptorType::COMBINED_IMAGE_SAMPLER,
+            DescriptorType::StorageBuffer => vk::DescriptorType::STORAGE_BUFFER,
+            DescriptorType::UniformBuffer => vk::DescriptorType::UNIFORM_BUFFER,
+        }
+    }
+
+    #[inline]
+    fn nova_stage_flags_to_vulkan_flags(stage_flags: ShaderStageFlags) -> vk::ShaderStageFlags {
+        // currently nova's ShaderStageFlags match their vulkan counterparts
+        stage_flags as vk::ShaderStageFlags
+    }
+
+    fn create_descriptor_set_layouts(
+        &self,
+        all_bindings: &HashMap<String, ResourceBindingDescription>,
+    ) -> Result<Vec<vk::DescriptorSetLayout>, MemoryError> {
+        let mut bindings_by_set = Vec::new();
+        for i in 0..all_bindings.len() {
+            bindings_by_set.push(Vec::new());
+        }
+
+        for (name, binding) in all_bindings {
+            if binding.set >= all_bindings.len() {
+                log::error!("Found skipped descriptor set, Nova can't handle that properly");
+                continue;
+            }
+
+            let descriptor_binding = vk::DescriptorSetLayoutBinding::builder()
+                .binding(binding.binding)
+                .descriptor_type(VulkanDevice::nova_descriptor_type_to_vulkan_type(
+                    binding.descriptor_type,
+                ))
+                .descriptor_count(binding.count)
+                .stage_flags(VulkanDevice::nova_stage_flags_to_vulkan_flags(binding.stages))
+                .build();
+
+            bindings_by_set.get_mut(binding.set).unwrap().push(descriptor_binding);
+        }
+
+        let mut layouts = Vec::new();
+        for bindings in bindings_by_set {
+            let create_info = vk::DescriptorSetLayoutCreateInfo::builder()
+                .bindings(bindings.as_slice())
+                .build();
+
+            layouts.push(
+                match unsafe { self.device.create_descriptor_set_layout(&create_info, None) } {
+                    Err(result) => {
+                        return match result {
+                            vk::Result::ERROR_OUT_OF_HOST_MEMORY => Err(MemoryError::OutOfHostMemory),
+                            vk::Result::ERROR_OUT_OF_DEVICE_MEMORY => Err(MemoryError::OutOfDeviceMemory),
+                            _ => panic!("Invalid vk result returned: {:?}", result),
+                        };
+                    }
+                    Ok(v) => v,
+                },
+            );
+        }
+
+        Ok(layouts)
+    }
+
     pub fn get_queue_families(&self) -> VulkanDeviceQueueFamilies {
         self.queue_families
     }
@@ -431,11 +495,135 @@ impl Device for VulkanDevice {
 
     fn create_pipeline_interface(
         &self,
-        bindings: &HashMap<String, ResourceBindingDescription, RandomState>,
+        bindings: &HashMap<String, ResourceBindingDescription>,
         color_attachments: &Vec<TextureAttachmentInfo>,
         depth_texture: &Option<TextureAttachmentInfo>,
     ) -> Result<Self::PipelineInterface, MemoryError> {
-        unimplemented!()
+        let layouts_by_set = self.create_descriptor_set_layouts(bindings)?;
+
+        let create_info = vk::PipelineLayoutCreateInfo::builder()
+            .set_layouts(layouts_by_set.as_slice())
+            .build();
+
+        let pipeline_layout = match unsafe { self.device.create_pipeline_layout(&create_info, None) } {
+            Err(result) => {
+                return match result {
+                    vk::Result::ERROR_OUT_OF_HOST_MEMORY => Err(MemoryError::OutOfHostMemory),
+                    vk::Result::ERROR_OUT_OF_DEVICE_MEMORY => Err(MemoryError::OutOfDeviceMemory),
+                    _ => panic!("Invalid vk result returned: {:?}", result),
+                };
+            }
+            Ok(v) => v,
+        };
+
+        let mut attachment_descriptions = Vec::new();
+        let mut attachment_references = Vec::new();
+
+        let mut writes_to_backbuffer = false;
+        for attachment in color_attachments {
+            match attachment.name.as_ref() {
+                "Backbuffer" => {
+                    writes_to_backbuffer = true;
+
+                    let attachment_description = vk::AttachmentDescription::builder()
+                        .format(self.swapchain.get_format())
+                        .samples(vk::SampleCountFlags::TYPE_1)
+                        .load_op(vk::AttachmentLoadOp::CLEAR)
+                        .store_op(vk::AttachmentStoreOp::STORE)
+                        .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
+                        .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
+                        .initial_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                        .final_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                        .build();
+
+                    attachment_descriptions.push(attachment_description);
+
+                    let attachment_reference = vk::AttachmentReference::builder()
+                        .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                        .attachment((attachment_descriptions.len() - 1) as u32)
+                        .build();
+
+                    attachment_references.push(attachment_reference);
+
+                    break;
+                }
+                _ => {
+                    let attachment_description = vk::AttachmentDescription::builder()
+                        .format(VulkanDevice::nova_pixel_format_to_vulkan_format(
+                            attachment.pixel_format,
+                        ))
+                        .samples(vk::SampleCountFlags::TYPE_1)
+                        .load_op(match attachment.clear {
+                            true => vk::AttachmentLoadOp::CLEAR,
+                            false => vk::AttachmentLoadOp::LOAD,
+                        })
+                        .store_op(vk::AttachmentStoreOp::STORE)
+                        .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
+                        .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
+                        .initial_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                        .final_layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                        .build();
+
+                    attachment_descriptions.push(attachment_description);
+
+                    let attachment_reference = vk::AttachmentReference::builder()
+                        .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL)
+                        .attachment((attachment_descriptions.len() - 1) as u32)
+                        .build();
+
+                    attachment_references.push(attachment_reference);
+                }
+            }
+        }
+
+        let depth_attachment_reference = depth_texture.map(|texture| {
+            let attachment_description = vk::AttachmentDescription::builder()
+                .format(self.nova_pixel_format_to_vulkan_format(texture.pixel_format))
+                .samples(vk::SampleCountFlags::TYPE_1)
+                .load_op(match texture.clear {
+                    true => vk::AttachmentLoadOp::CLEAR,
+                    false => vk::AttachmentLoadOp::LOAD,
+                })
+                .store_op(vk::AttachmentStoreOp::STORE)
+                .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
+                .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
+                .initial_layout(vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+                .final_layout(vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+                .build();
+
+            attachment_descriptions.push(attachment_description);
+
+            vk::AttachmentReference::builder()
+                .layout(vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+                .attachment((attachment_descriptions.len() - 1) as u32)
+                .build()
+        });
+
+        let subpass_description = vk::SubpassDescription::builder()
+            .pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS)
+            .color_attachments(attachment_references.as_slice())
+            .build();
+
+        let image_available_dependency = vk::SubpassDependency::builder()
+            .src_subpass(vk::SUBPASS_EXTERNAL)
+            .dst_subpass(0)
+            .src_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+            .src_access_mask(0 as vk::AccessFlags)
+            .dst_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+            .dst_access_mask(vk::AccessFlags::COLOR_ATTACHMENT_READ | vk::AccessFlags::COLOR_ATTACHMENT_WRITE)
+            .build();
+
+        let render_pass_create_info = vk::RenderPassCreateInfo::builder()
+            .subpasses(&[subpass_description])
+            .dependencies(&[image_available_dependency])
+            .attachments(attachment_descriptions.as_slice())
+            .build();
+
+        Ok(VulkanPipelineInterface {
+            vk_pipeline_layout: pipeline_layout,
+            bindings: bindings.clone(),
+            layouts_by_set,
+        })
     }
 
     fn create_descriptor_pool(
--- src/rhi/vulkan/vulkan_pipeline_interface.rs
+++ src/rhi/vulkan/vulkan_pipeline_interface.rs
@@ -1,9 +1,12 @@
 use crate::rhi::*;
 
 use ash::vk;
+use std::collections::HashMap;
 
 pub struct VulkanPipelineInterface {
     pub vk_pipeline_layout: vk::PipelineLayout,
+    pub bindings: HashMap<String, ResourceBindingDescription>,
+    pub layouts_by_set: Vec<vk::DescriptorSetLayout>,
 }
 
 impl PipelineInterface for VulkanPipelineInterface {}
