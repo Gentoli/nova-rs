37c487d336a268e17c5b242d5051c4456c5bc2e5
David Dubois <dexcelstraun7@gmail.com>
2019-08-16 20:44:04 -0700

    [rhi::dx12] Pulled some stuff into a different function

--- src/rhi/dx12/dx12_utils.rs
+++ src/rhi/dx12/dx12_utils.rs
@@ -16,6 +16,7 @@ use winapi::shared::winerror::FAILED;
 use winapi::um::d3d12shader::*;
 use winapi::um::d3dcompiler::*;
 
+use std::ffi::CStr;
 use std::mem;
 use winapi::Interface;
 
@@ -31,7 +32,7 @@ pub fn compile_shader(
     shader: shaderpack::ShaderSource,
     target: String,
     options: hlsl::CompilerOptions,
-    tables: &HashMap<u32, Vec<D3D12_DESCRIPTOR_RANGE1>>,
+    tables: &mut HashMap<u32, Vec<D3D12_DESCRIPTOR_RANGE1>>,
 ) -> Result<WeakPtr<ID3DBlob>, spirv_cross::ErrorCode> {
     let shader_module = spirv::Module::from_words(&shader.source);
     let shader_compiler = spirv::Ast::<hlsl::Target>::parse(&shader_module);
@@ -57,8 +58,8 @@ pub fn compile_shader(
                     D3DCompile2(
                         shader_hlsl.as_ptr() as _,
                         shader_hlsl.len(),
-                        shader.filename.into_os_string().as_ptr() as _,
-                        null,
+                        shader.filename.to_str().unwrap().as_ptr() as _,
+                        null as _,
                         D3D_COMPILE_STANDARD_FILE_INCLUDE,
                         "main".as_ptr() as _,
                         target.as_ptr() as _,
@@ -114,12 +115,15 @@ pub fn compile_shader(
                         )));
                     }
 
-                    let (descriptor_type, spirv_resource, set) = match binding_desc.Type {
-                        D3D12_SIT_CBUFFER => {
-                            let descriptor_type = D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
-                            let spirv_resource = spirv_uniform_buffers[binding_desc.Name];
-                            let set = shader_compiler.get_decoration(spirv_resource.id, spv::DecorationDescriptorSet);
-                        }
+                    let (descriptor_type, spirv_resource, set) = match get_descriptor_info(
+                        tables,
+                        &shader_compiler.unwrap(),
+                        &spirv_sampled_images,
+                        &spirv_uniform_buffers,
+                        &mut binding_desc,
+                    ) {
+                        Ok(tup) => tup,
+                        Err(e) => return Err(e),
                     };
                 }
 
@@ -143,4 +147,69 @@ pub fn compile_shader(
     let blob = WeakPtr::<ID3DBlob>::null();
 
     Ok(blob)
+}
+
+fn get_descriptor_info(
+    tables: &mut HashMap<u32, Vec<D3D12_DESCRIPTOR_RANGE1>>,
+    shader_compiler: &spirv::Ast<hlsl::Target>,
+    mut spirv_sampled_images: &HashMap<String, spirv::Resource>,
+    mut spirv_uniform_buffers: &HashMap<String, spirv::Resource>,
+    mut binding_desc: &mut D3D12_SHADER_INPUT_BIND_DESC,
+) -> Result<(u32, spirv::Resource, u32), ErrorCode> {
+    // if binding_desc.Type
+
+    match binding_desc.Type {
+        D3D12_SIT_CBUFFER => {
+            let name_str = unsafe { CStr::from_ptr(binding_desc.Name as _) }.to_str().unwrap();
+            let descriptor_type = D3D12_DESCRIPTOR_RANGE_TYPE_CBV;
+            let spirv_resource = spirv_uniform_buffers[name_str].clone();
+            let set = match shader_compiler.get_decoration(spirv_resource.id, spirv::Decoration::DescriptorSet) {
+                Ok(set) => set,
+                Err(e) => {
+                    return Err(ErrorCode::CompilationError(String::from(
+                        "Could not get descriptor set decoration",
+                    )));
+                }
+            };
+
+            add_resource_to_descriptor_table(&descriptor_type, &binding_desc, &set, tables);
+
+            Ok((descriptor_type, spirv_resource, set))
+        }
+        D3D_SIT_TEXTURE => {
+            let name_str = unsafe { CStr::from_ptr(binding_desc.Name as _) }.to_str().unwrap();
+            let descriptor_type = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+            let spirv_resource = spirv_sampled_images[name_str].clone();
+            let set = match shader_compiler.get_decoration(spirv_resource.id, spirv::Decoration::DescriptorSet) {
+                Ok(set) => set,
+                Err(e) => {
+                    return Err(ErrorCode::CompilationError(String::from(
+                        "Could not get descriptor set decoration",
+                    )));
+                }
+            };
+
+            add_resource_to_descriptor_table(&D3D12_DESCRIPTOR_RANGE_TYPE_SAMPLER, &binding_desc, &set, &mut tables);
+
+            Ok((descriptor_type, spirv_resource, set))
+        }
+    }
+}
+
+fn add_resource_to_descriptor_table(
+    descriptor_type: &u32,
+    binding_desc: &D3D12_SHADER_INPUT_BIND_DESC,
+    set: &u32,
+    tables: &mut HashMap<u32, Vec<D3D12_DESCRIPTOR_RANGE1>>,
+) {
+    let range = D3D12_DESCRIPTOR_RANGE1 {
+        RangeType: descriptor_type.clone(),
+        NumDescriptors: 1,
+        BaseShaderRegister: binding_desc.BindPoint,
+        RegisterSpace: binding_desc.Space,
+        Flags: 0,
+        OffsetInDescriptorsFromTableStart: D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND,
+    };
+
+    tables[set].push(range);
 }
