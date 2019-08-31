e6d8bcccbebac61171fb46d073fd3caf28028693
David Dubois <dexcelstraun7@gmail.com>
2019-08-31 10:29:36 -0700

    [rhi::dx12] Dx12Buffer

--- src/rhi/dx12/dx12_buffer.rs
+++ src/rhi/dx12/dx12_buffer.rs
@@ -1,5 +1,9 @@
+#![allow(unsafe_code)]
+
 use crate::rhi::dx12::com::WeakPtr;
-use crate::rhi::{Buffer, BufferCreateInfo};
+use crate::rhi::{Buffer, BufferCreateInfo, MappingError};
+use core::ptr;
+use winapi::shared::winerror::SUCCEEDED;
 use winapi::um::d3d12::*;
 
 pub struct Dx12Buffer {
@@ -8,7 +12,30 @@ pub struct Dx12Buffer {
 }
 
 impl Buffer for Dx12Buffer {
-    fn write_data(&self, data: BufferCreateInfo, num_bytes: u64, offset: u64) {
-        unimplemented!()
+    fn map(&self) -> Result<*mut (), MappingError> {
+        let mapped_range = D3D12_RANGE {
+            Begin: 0 as usize,
+            End: size as usize,
+        };
+
+        let mut mapped_buffer = ptr::null_mut();
+
+        unsafe {
+            let hr = self.resource.Map(0, &mapped_range, &mapped_buffer);
+            if SUCCEEDED(hr) {
+                Ok(mapped_buffer)
+            } else {
+                Err(MappingError::MappingFailed)
+            }
+        }
+    }
+
+    fn unmap(&self) {
+        let mapped_range = D3D12_RANGE {
+            Begin: 0 as usize,
+            End: size as usize,
+        };
+
+        unsafe { self.resource.Unmap(0, &mapped_range) };
     }
 }
