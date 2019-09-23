5f744ddbd5c424a4cc702d8782f950b4d4c3aeac
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-09-23 15:23:44 -0400

    [cargo] Purge all duplicate dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,41 @@
 //! This is a rewrite of the old [Nova Renderer](https://github.com/NovaMods/nova-renderer) project
 //! from C++ to Rust.
 
+// Rust features
 #![feature(async_await)]
 #![feature(async_closure)]
 #![feature(seek_convenience)]
 #![feature(test)]
-#![feature(type_alias_impl_trait)]
-#![allow(clippy::cognitive_complexity)]
-#![allow(clippy::float_cmp)]
-#![deny(nonstandard_style)]
-#![deny(future_incompatible)]
-#![deny(rust_2018_idioms)]
-#![deny(unsafe_code)]
-#![warn(missing_docs)]
+// Rust warnings
 #![warn(unused)]
+#![deny(future_incompatible)]
+#![deny(nonstandard_style)]
+#![deny(rust_2018_idioms)]
+#![deny(unsafe_code)] // Most is safe, but the RHI needs unsafe
+// Clippy warnings
+#![warn(clippy::cargo)]
+#![warn(clippy::nursery)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::restriction)]
+// Annoying regular clippy warnings
+#![allow(clippy::cast_sign_loss)] // Annoying
+#![allow(clippy::cast_precision_loss)] // Annoying
+#![allow(clippy::cast_possible_truncation)] // Annoying
+#![allow(clippy::cognitive_complexity)] // This is dumb
+// Annoying/irrelevant clippy Restrictions
+#![allow(clippy::decimal_literal_representation)]
+#![allow(clippy::else_if_without_else)]
+#![allow(clippy::float_arithmetic)]
+#![allow(clippy::float_cmp_const)]
+#![allow(clippy::implicit_return)]
+#![allow(clippy::integer_arithmetic)]
+#![allow(clippy::integer_division)]
+#![allow(clippy::mem_forget)] // Useful for FFI
+#![allow(clippy::missing_docs_in_private_items)]
+#![allow(clippy::missing_inline_in_public_items)]
+#![allow(clippy::shadow_reuse)]
+#![allow(clippy::shadow_same)]
+#![allow(clippy::wildcard_enum_match_arm)]
 
 pub mod async_utils;
 pub mod core;
